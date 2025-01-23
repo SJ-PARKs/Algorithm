ca7b282a6dfb1c8c364c25fabc3621bbfc8503bf
박성준 <106297379+SJ-PARKs@users.noreply.github.com>
2025-01-23 15:02:02 +0900

    Update 겹치는 건 싫어.py

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -19,4 +19,27 @@ while s<len(arr):
 		maximum=s-e
 print(maximum)
 
-		
+# n,k=map(int,input().split())
+# arr=list(map(int,input().split()))
+
+# p1,p2=0,0
+# answer=0
+# count=dict()
+
+# def add_count(x):
+# 	if x not in count:
+# 		count[x]=0
+# 	count[x]+=1
+
+# while p2<len(arr):
+# 	if arr[p2] in count and count[arr[p2]]>=k:
+# 		while count[arr[p1]]==k:
+# 			count[arr[p1]]-=1
+# 			p1+=1
+# 	else:
+# 		add_count(arr[p2])
+# 		p2+=1
+# 	if p2-p1>answer:
+# 		answer=p2-p1
+
+# print(answer)
