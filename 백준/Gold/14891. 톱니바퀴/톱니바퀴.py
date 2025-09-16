12241939a9cb747b12c8345b62b044a114707708
박성준 <106297379+SJ-PARKs@users.noreply.github.com>
2025-09-16 20:57:11 +0900

    [Gold V] Title: 톱니바퀴, Time: 112 ms, Memory: 112308 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,43 +1,41 @@
-import collections
-s = [] #톱니바퀴배열 
-for _ in range(4):
-    s.append(collections.deque(list(input())))
-K = int(input()) #회전횟수
-R = [list(map(int, input().split())) for _ in range(K)] 
+from collections import deque
 
-#왼쪽 톱니바퀴 확인
-def left(num, direction): 
-    if num < 0: #첫번째톱는 확인안함
-        return 
-    if s[num][2] != s[num+1][6]: #극이 다른경우
-        left(num-1, -direction)  #그 왼쪽 톱니바퀴도 조사 
-        s[num].rotate(direction) #현재 톱니바퀴는 회전 
+s=[]
+for i in range(4):
+	s.append(deque(list(map(int,input()))))
+	
+k=int(input())
+spin=[]
+for i in range(k):
+	spin.append(list(map(int,input().split())))
 
-#오른쪽 톱니바퀴 확인
-def right(num, direction): 
-    if num > 3: #마지막은 확인안함
-        return 
-    if s[num][6] != s[num-1][2]: #극이 다른경우
-        right(num+1, -direction) #그 오른족 톱니바퀴도 조사
-        s[num].rotate(direction) #현재 톱니바퀴는 회전
-
-
-for i in range(K): 
-    num = R[i][0] - 1 #돌아가는 톱니바퀴
-    direction = R[i][1] #시계, 반시계방향
-    left(num-1, -direction) #왼쪽조사
-    right(num+1, -direction) #오른쪽조사
-    s[num].rotate(direction) #현재 톱니바퀴는 회전
-
-res = 0 #점수
-
-if s[0][0] == '1':
-    res += 1
-if s[1][0] == '1':
-    res += 2
-if s[2][0] == '1':
-    res += 4
-if s[3][0] == '1':
-    res += 8
-    
+def left(n,d):
+	if n<0:
+		return
+	if s[n][2]!=s[n+1][6]:
+		left(n-1,-d)
+		s[n].rotate(d)
+	
+def right(n,d):
+	if n>3:
+		return
+	if s[n][6]!=s[n-1][2]:
+		right(n+1,-d)
+		s[n].rotate(d)
+	
+for i in range(k):
+	right(spin[i][0],-spin[i][1])
+	left(spin[i][0]-2,-spin[i][1])
+	s[spin[i][0]-1].rotate(spin[i][1])
+	
+res =0
+if s[0][0]==1:
+	res+=1
+if s[1][0]==1:
+	res+=2
+if s[2][0]==1:
+	res+=4
+if s[3][0]==1:
+	res+=8
+	
 print(res)
