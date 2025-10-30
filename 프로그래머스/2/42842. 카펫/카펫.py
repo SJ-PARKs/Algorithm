09206aa55359f50a83574b8d344220c13bbaf16c
박성준 <106297379+SJ-PARKs@users.noreply.github.com>
2025-10-30 13:56:30 +0900

    [level 2] Title: 카펫, Time: 0.10 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,11 +1,10 @@
 def solution(brown, yellow):
     answer = []
     hap=brown+yellow
-    for i in range(1,brown//2):
-        a=i
-        b=brown//2-i
-        if (a-1)*(b-1)==yellow:
-            answer.append(b+1)
-            answer.append(a+1)
-            break
+    for i in range(1,hap//2+1):
+        if hap%i==0:
+            if (i-2)*(hap//i-2)==yellow:
+                answer.append(max(i,hap//i))
+                answer.append(min(i,hap//i))  
+                break
     return answer
