b456d5fa04c3652b3a6473a047869f4340835118
박성준 <106297379+SJ-PARKs@users.noreply.github.com>
2025-10-30 20:14:24 +0900

    [level 3] Title: 이중우선순위큐, Time: 1782.01 ms, Memory: 15.7 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -2,29 +2,28 @@ import heapq
 
 def solution(operations):
     answer = []
-    heap=[]
+    q=[]
+    heapq.heapify(q)    
     for i in range(len(operations)):
-        operation,num=operations[i].split(" ")
-        if operation=="I":
-            heapq.heappush(heap,int(num))
-        if operation=="D" and num=="-1":
-            if len(heap)==0:
-                continue
-            heapq.heappop(heap)
-        if operation=="D" and num=="1":
-            if len(heap)==0:
-                continue
-            heap=sorted(heap)
-            heap.pop(-1)
-            heapq.heapify(heap)
+        command=operations[i].split(" ")[0]
+        number=operations[i].split(" ")[1]
+        if command=="I":
+            heapq.heappush(q,int(number))
+        if len(q)==0:
+            continue
+        if command=="D" and number=="-1":
+            heapq.heappop(q)
+        if command=="D" and number=="1":
+            q=list(map(lambda x:-x,q))
+            heapq.heapify(q)
+            heapq.heappop(q)
+            q=list(map(lambda x:-x,q))
+            heapq.heapify(q)
+        
+        
+    if len(q)==0:
+        return [0,0]
+    answer.append(max(q))
+    answer.append(min(q))
     
-    if len(heap)==0:
-        answer.append(0)
-        answer.append(0)
-    if len(heap)==1:
-        answer.append(heap[0])
-        answer.append(heap[0])
-    if len(heap)>=2:
-        answer.append(max(heap))
-        answer.append(min(heap))
     return answer
