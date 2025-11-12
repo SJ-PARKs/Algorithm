1b391b5aa4ba36eb05bddb59488ef4183965a784
박성준 <106297379+SJ-PARKs@users.noreply.github.com>
2025-11-12 19:44:54 +0900

    [Silver I] Title: 포도주 시식, Time: 116 ms, Memory: 111060 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,19 +1,18 @@
 n=int(input())
-arr=[]
-dp=[0 for _ in range(n)]
+
+arr=[0]*10001
+dp=[0]*10001
+
 for i in range(n):
-	arr.append(int(input()))
-
-if n==1:
-	print(arr[0])
-elif n==2:
-	print(arr[0]+arr[1])
-else:		
-	dp[0]=arr[0]
-	dp[1]=arr[0]+arr[1]			    
-	dp[2]=max(max(arr[0]+arr[1],arr[1]+arr[2]),arr[0]+arr[2])
-
-	for i in range(3,n):
-		dp[i]=max(max(dp[i-3]+arr[i-1]+arr[i],dp[i-1]),dp[i-2]+arr[i])
+	arr[i]=int(input())
 	
-	print(dp[n-1])
+dp[0]=arr[0]
+dp[1]=arr[0]+arr[1]
+dp[2]=max(max(dp[1],arr[0]+arr[2]),arr[1]+arr[2])
+
+for i in range(3,n):
+	dp[i]=max(max(dp[i-3]+arr[i-1]+arr[i],dp[i-2]+arr[i]),dp[i-1])
+
+print(dp[n-1])
+	
+	
