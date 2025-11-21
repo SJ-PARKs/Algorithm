15f537b686b303436a282f88f7f55312b4378314
박성준 <106297379+SJ-PARKs@users.noreply.github.com>
2025-11-21 21:52:36 +0900

    [level 3] Title: 입국심사, Time: 554.74 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,17 +1,15 @@
 def solution(n, times):
     answer = 0
     l=0
-    r= 1000000000000000001
-    mid=0
+    r=max(times)*n
     while l<=r:
         mid=(l+r)//2
         people=0
         for time in times:
-            people+=mid//time
-
-        if people<n:
-            l=mid+1
-        else:
+            people+=(mid//time)
+        if people>=n:
+            answer=mid
             r=mid-1
-  
-    return l
+        else:
+            l=mid+1
+    return answer
