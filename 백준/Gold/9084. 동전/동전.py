057f8fa48a100decff81fb906a2fd5d91b396ffe
박성준 <106297379+SJ-PARKs@users.noreply.github.com>
2025-12-16 23:01:36 +0900

    [Gold V] Title: 동전, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -1,16 +1,12 @@
 t=int(input())
 for _ in range(t):
-	n=int(input())
-	coin=list(map(int,input().split()))
-	m=int(input())
-	dp=[[0 for _ in range(m+1)] for _ in range(n+1)]
-	dp[0][0] = 1
-	for i in range(1,n+1):
-		for j in range(m+1):
-			dp[i][j]=dp[i-1][j]
-		for j in range(coin[i-1],m+1):
-			dp[i][j]=dp[i-1][j]+dp[i][j-coin[i-1]]
-	
-	print(dp[n][m])
-	
-	
+    n=int(input())
+    coin=list(map(int,input().split()))
+    m=int(input())
+    dp=[0]*(m+1)
+    dp[0]=1
+    for i in range(n):
+        for j in range(coin[i],m+1):
+            dp[j]=dp[j-coin[i]]+dp[j]
+    print(dp[m])
+
