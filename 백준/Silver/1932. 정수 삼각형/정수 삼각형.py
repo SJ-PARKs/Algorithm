29aa3aa51032bbf563a6fabe9564a4144aaefd2a
박성준 <106297379+SJ-PARKs@users.noreply.github.com>
2026-01-03 21:19:15 +0900

    [Silver I] Title: 정수 삼각형, Time: 120 ms, Memory: 111668 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,23 +1,20 @@
 n=int(input())
 arr=[]
 dp=[]
+for i in range(n):
+    arr.append(list(map(int,input().split())))
 
 for i in range(n):
-	arr.append(list(map(int,input().split())))
-	dp.append([0 for _ in range(i+1)])
+    dp.append([0]*(i+1))
 
 dp[0][0]=arr[0][0]
-	
+
 for i in range(1,n):
-	dp[i][0]=dp[i-1][0]+arr[i][0]
-	dp[i][i]=dp[i-1][i-1]+arr[i][i]
+    dp[i][0]=dp[i-1][0]+arr[i][0]
+    dp[i][i]=dp[i-1][i-1]+arr[i][i]
 
 for i in range(2,n):
-	for j in range(1,i):
-		dp[i][j]=max(dp[i-1][j-1],dp[i-1][j])+arr[i][j]
+    for j in range(1,i):
+        dp[i][j]=max(dp[i-1][j-1],dp[i-1][j])+arr[i][j]
 
-answer=0
-for i in range(n):
-	answer=max(answer,dp[n-1][i])
-	
-print(answer)
+print(max(dp[n-1]))
