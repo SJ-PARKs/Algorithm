24190f82b4745ebe6868cc5bba057f68103b1cc5
박성준 <106297379+SJ-PARKs@users.noreply.github.com>
2026-03-02 01:10:37 +0900

    [Gold V] Title: 회의실 배정, Time: 300 ms, Memory: 120288 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,17 +1,16 @@
 n=int(input())
-time=[]
-for _ in range(0,n):
-	a,b=map(int,input().split())
-	time.append((a,b))
-
-time= sorted(time, key=lambda x:x[0])
-time= sorted(time, key=lambda x:x[1])
-
-count=0
-end_time=0
+arr=[]
+answer=0
 for i in range(n):
-	if end_time<=time[i][0]:
-		end_time=time[i][1]
-		count+=1
-		
-print(count)
+    a,b=map(int,input().split())
+    arr.append((a,b))
+    
+arr.sort(key=lambda x:x[0])
+arr.sort(key=lambda x:x[1])
+
+end=0
+for i in range(len(arr)):
+    if end<=arr[i][0]:
+        end=arr[i][1]
+        answer+=1
+print(answer)
