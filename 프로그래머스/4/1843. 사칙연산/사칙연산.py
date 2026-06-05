d4cd25f4b01f28e41a5bf5721476d40768e94271
박성준 <106297379+SJ-PARKs@users.noreply.github.com>
2026-06-05 17:18:43 +0900

    [level 4] Title: 사칙연산, Time: 47.57 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.py
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.py
@@ -1,17 +1,18 @@
-INF = 1e9
-
 def solution(arr):
+    answer = -1
     numbers=[int(arr[i]) for i in range(0,len(arr),2)]
-    operators=[arr[i] for i in range(1,len(arr)-1,2)]
+    operators=[arr[i] for i in range(1,len(arr),2)]
+    
+    max_dp=[[-float('inf') for _ in range(len(numbers))] for _ in range(len(numbers))]
+    min_dp=[[float('inf') for _ in range(len(numbers))] for _ in range(len(numbers))]
     
     n=len(numbers)
     
-    max_dp=[[-INF]*n for _ in range(n)] 
-    min_dp=[[INF]*n for _ in range(n)]
-    
     for i in range(n):
-        max_dp[i][i]=min_dp[i][i]=numbers[i]
-    
+        max_dp[i][i]=numbers[i]
+        min_dp[i][i]=numbers[i]
+
+    print(max_dp)
     for size in range(1,n):
         for i in range(n-size):
             j=i+size
@@ -23,5 +24,6 @@ def solution(arr):
                     max_dp[i][j]=max(max_dp[i][j],max_dp[i][k]-min_dp[k+1][j])
                     min_dp[i][j]=min(min_dp[i][j],min_dp[i][k]-max_dp[k+1][j])
                     
+    answer=max_dp[0][-1]            
     
-    return max_dp[0][n-1]
+    return answer
