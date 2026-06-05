8b92b20795727b22289001002e8155aeb92bc20b
박성준 <106297379+SJ-PARKs@users.noreply.github.com>
2026-06-05 16:18:36 +0900

    [level 3] Title: N으로 표현, Time: 29.88 ms, Memory: 15.9 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,22 +1,24 @@
 def solution(N, number):
-    if N==number:
-        return 1
-    
-    dp=[set() for _ in range(9)]
-    
-    for i in range(1,9):
+    answer = -1
+    dp=[set() for _ in range(10)]
+
+    for i in range(1,10):
         dp[i].add(int(str(N)*i))
-        
-        for j in range(1,i):
-            for a in dp[j]:
-                for b in dp[i-j]:
-                    dp[i].add(a+b)
-                    dp[i].add(a-b)
-                    dp[i].add(a*b)
-                    if b!=0:
-                        dp[i].add(a//b)
-        
-        if number in dp[i]:
-            return i
     
-    return -1
+    for count in range(2,10):
+        for left_count in range(1,count):
+            right_count=count-left_count
+            for i in dp[left_count]:
+                for j in dp[right_count]:
+                    dp[count].add(i+j)
+                    dp[count].add(i-j)
+                    dp[count].add(i*j)
+                    if j!=0:
+                        dp[count].add(i//j)
+    
+    for i in range(1,10):
+        if number in dp[i]:
+            answer=i
+            break
+            
+    return answer
