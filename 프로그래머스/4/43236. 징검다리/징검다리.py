a33311639156550cd22ed47fac3001d829d82218
박성준 <106297379+SJ-PARKs@users.noreply.github.com>
2026-06-06 10:25:50 +0900

    [level 4] Title: 징검다리, Time: 83.54 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,35 +1,31 @@
 def solution(distance, rocks, n):
     answer = 0
-    left,right=0,distance
+    num=len(rocks)
+    
     rocks.sort()
-    while left<=right:
-        mid=(left+right)//2
-        if check(mid,rocks,distance)>n:
-            right=mid-1
-        else:
+    
+    l,r=0,1000000000
+    
+    while l<=r:
+        mid=(l+r)//2
+        remove_num=remove_rocks(rocks,mid,distance)
+        if remove_num<=n:
             answer=mid
-            left=mid+1
-            
-            
-        
+            l=mid+1
+        else:
+            r=mid-1
+    
     return answer
 
-def check(mid,rocks,distance):
+def remove_rocks(rocks,shortest_len,distance):
     before=0
     count=0
+    end=distance
     for i in range(len(rocks)):
-        if rocks[i]-before<mid:
+        if shortest_len>rocks[i]-before:
             count+=1
-            continue
-        before=rocks[i]
-    
-    if distance-before<mid:
-        count+=1
+        else:
+            before=rocks[i]
+    if end-before<shortest_len: count+=1
     
     return count
-        
-    
-        
-            
-    
-    
